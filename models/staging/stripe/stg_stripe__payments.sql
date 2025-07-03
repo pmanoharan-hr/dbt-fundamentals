b9d12d98873394b81cfdf9aa50502d552ef43fdc
Prakash manoharan <pmanoharan@harryrosen.com>
2025-07-03 20:06:16 +0000

    Including package usage

--- models/staging/stripe/stg_stripe__payments.sql
+++ models/staging/stripe/stg_stripe__payments.sql
@@ -4,7 +4,7 @@ id as payment_id,
 orderid as order_id,
 paymentmethod as payment_method,
 status,
-amount/100 as amount,
+{{ cents_to_dollars('amount', 4)}} as amount,
 created as created_at
 
 from {{ source('stripe', 'stripe_payments') }}
